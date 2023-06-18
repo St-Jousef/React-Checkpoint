55367e8dc4aeb0de34cd673ce7f1ec9ee8078ae3
St-Jousef <Joseph_nkwocha>
2023-06-18 18:04:58 +0100

    Second commit

--- src/index.js
+++ src/index.js
@@ -9,13 +9,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
+      
+    <div className='form-container'>
     <Forms/>
+    </div>
+    
   </React.StrictMode>
 );
 
