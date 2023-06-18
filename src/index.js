69764372edcb1c8ffcf85915328dd15cd7e57b3f
St-Jousef <Joseph_nkwocha>
2023-06-18 17:12:26 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Forms from './Form';
+import reportWebVitals from './reportWebVitals';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <Forms/>
+  </React.StrictMode>
+);
+
+
+reportWebVitals();
